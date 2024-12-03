a505932474fabd38e814d94a6da883cd5131aac1
MarcoMangia <marco.mangia@softagile.com>
2024-12-03 17:29:41 +0100

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DECO2.AdventOfCode02bis();
+        }
+    }
+}
